c36080d94e0cd7fdd8c46898d0f2df977e406ca3
Yugal109 <yugalkhati570@gmail.com>
2021-07-06 21:37:38 +0545

    running

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ app.get("/",(req,res)=>{
 })
 
 
-const PORT=5000;
-app.listen(PORT,()=>{
-    console.log(`Server started at port ${PORT}.`)
+
+
+app.listen(5000,()=>{
+    console.log(`Server started at port 5000.`)
 })
