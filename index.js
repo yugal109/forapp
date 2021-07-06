c48fb05b18be729c1a4d7a91db62795aaf741399
Yugal109 <yugalkhati570@gmail.com>
2021-07-06 21:15:18 +0545

    inisitalizing everything

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require("express")
+const app=express()
+
+app.get("",(req,res)=>{
+    res.send("Hey man.")
+})
+
+
+const PORT=5000;
+app.listen(PORT,()=>{
+    console.log(`Server started at port ${PORT}.`)
+})
