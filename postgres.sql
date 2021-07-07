26d910317b4631eb3a7ab46d4ad40f5e2cc229c2
Yugal109 <yugalkhati570@gmail.com>
2021-07-07 09:52:41 +0545

    added

--- postgres.sql
+++ postgres.sql
@@ -1,4 +1,9 @@
 
 CREATE TABLE Users(
-    -- id BIGSERIAL PRIMARY KEY 
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    username VARCHAR(100),
+    email VARCHAR(150),
+    password VARCHAR(250)
 )
